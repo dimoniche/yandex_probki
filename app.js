dc21c78431780eea64f45d05c43b17cd34848d1d
dimoniche <dimoniche@gmail.com>
2013-06-04 02:42:01 +0400

    еще немного

--- app.js
+++ app.js
@@ -3,33 +3,21 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+//var express = require('express')
+//var routes = require('./routes')
+//var user = require('./routes/user')
+var http = require('http')
+var path = require('path');
 
 var net  = require('net');
 var mpns = require('mpns');
 
-var app = express();
-
 // all environments
-app.set('port', 5002);
-app.set('port_phone7', 5003);
-
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var port = 5002;
+var port_phone7 = 5003;
 
 // База данных
-var databaseUrl = 'dimoniche.cloudapp.net/tile_update_test';
+var databaseUrl = 'dimoniche.cloudapp.net/tile_update';
 
 var collections      = ["users"];
 var town_collections = ["town"];
@@ -39,14 +27,6 @@ var mongojs = require('mongojs');
 var db = mongojs(databaseUrl,collections);
 var db_town = mongojs(databaseUrl,town_collections);
 var db_stat = mongojs(databaseUrl,stat_collections);
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
 
 var TextBoxCount = 0;
 
@@ -65,18 +45,8 @@ var Red    = "http://dimoniche.cloudapp.net/svetofor_red.png";
 var count_user;
 
 // обновление плитки
-function update_tile(URI)
+function update_tile(URI,options)
 {
-    var options = {
-
-        backgroundImage: TextBoxBackgroundImage,
-        count: TextBoxCount,
-        title: TextBoxTitle,
-        backBackgroundImage: TextBoxBackBackgroundImage,
-        backTitle: TextBoxBackTitle,
-        backContent: TextBoxBackContent
-    };
-
     mpns.sendTile(URI,options);
 
     console.log('обновляем тайл');
@@ -88,6 +58,15 @@ var intervalID = setInterval(function()
 
         // спросим у яндекса как там с пробками дела
         var jsonobj;        // объект со всеми пробками из яндекса
+        var options = {
+
+            backgroundImage: TextBoxBackgroundImage,
+            count: TextBoxCount,
+            title: TextBoxTitle,
+            backBackgroundImage: TextBoxBackBackgroundImage,
+            backTitle: TextBoxBackTitle,
+            backContent: TextBoxBackContent
+        };
 
         http.get("http://api-maps.yandex.ru/services/traffic-info/1.0/?format=json",
             function(res) {
@@ -106,73 +85,79 @@ var intervalID = setInterval(function()
 
                     //  смотрим по базе всех пользователей
                     db.users.find(function(err, users) {
-                        for(var i = 0; i < users.length; i++)
+                        if(users != undefined)
                         {
-                            var number_town = 0;
+                            for(var i = 0; i < users.length; i++)
+                            {
+                                db_town.town.find({name: users[i].Town}, function(err, town) {
 
-                            db_town.town.find({name: jsonobj.GeoObjectCollection.features[i].properties.name.toString()}, function(err, town) {
-                                if( err || !town.length)
-                                {   // no town
+                                    if( err || !town.length)
+                                    {   // no town
+                                        console.log("Ошибка " + err);
+                                        console.log(town.length);
+                                    }
+                                    else
+                                    {   // пользователь есть - обновим координаты и город
+                                        if(town[0].number != undefined){number_town = town[0].number;}
+                                        else                           {number_town = 0;}
 
-                                }
-                                else town.forEach( function(Town) {
-                                    // пользователь есть - обновим координаты и город
-                                    if(Town.number !== undefined){number_town = Town.number;}
-                                    else                         {number_town = 0;}
+                                        console.log(number_town);
+                                        console.log(town[0].name);
+
+                                        var level = jsonobj.GeoObjectCollection.features[number_town].properties.JamsMetaData.level;
+                                        var icon  = jsonobj.GeoObjectCollection.features[number_town].properties.JamsMetaData.icon;
+                                        var name  = jsonobj.GeoObjectCollection.features[number_town].properties.name;
+
+                                        if(level != undefined) {;}
+                                        else                  {level = 1;}
+
+                                        if(icon != undefined) {;}
+                                        else                  {icon = 'green';}
+
+                                        options.title       = level.toString();
+                                        options.backTitle   = ' ';
+                                        options.backContent = ' ';
+
+                                        switch(icon)
+                                        {
+                                            case "green":
+                                                options.backgroundImage = Green;
+                                                break;
+                                            case "yellow":
+                                                options.backgroundImage = Yellow;
+                                                break;
+                                            case "red":
+                                                options.backgroundImage = Red;
+                                                break;
+                                        }
+
+                                        switch(name)
+                                        {
+                                            case "Санкт-Петербург":
+                                                options.backBackgroundImage = 'http://info.maps.yandex.net/traffic/spb/tends_200.png';
+                                                break;
+                                            case "Москва":
+                                                options.backBackgroundImage = 'http://info.maps.yandex.net/traffic/moscow/tends_200.png';
+                                                break;
+                                            case "Екатеринбург":
+                                                options.backBackgroundImage = 'http://info.maps.yandex.net/traffic/ekb/tends_200.png';
+                                                break;
+                                            case "Киев":
+                                                options.backBackgroundImage = 'http://info.maps.yandex.net/traffic/kiev/tends_200.png';
+                                                break;
+                                            default :
+                                                options.backBackgroundImage = '';
+                                                break;
+                                        }
+
+                                        update_tile(users[i].URIs,options);
+
+                                        //console.log(users[i]);
+                                        console.log(options.backgroundImage);
+                                        console.log(options.backBackgroundImage);
+                                    }
                                 });
-                            });
-
-                            var level = jsonobj.GeoObjectCollection.features[number_town].properties.JamsMetaData.level;
-                            var icon  = jsonobj.GeoObjectCollection.features[number_town].properties.JamsMetaData.icon;
-                            var name  = jsonobj.GeoObjectCollection.features[number_town].properties.name;
-
-                            if(level != undefined) {;}
-                            else                  {level = 1;}
-
-                            if(icon != undefined) {;}
-                            else                  {icon = 'green';}
-
-                            TextBoxTitle       = level.toString();
-                            TextBoxBackTitle   = ' ';
-                            TextBoxBackContent = ' ';
-
-                            switch(icon)
-                            {
-                                case "green":
-                                    TextBoxBackgroundImage = Green;
-                                break;
-                                case "yellow":
-                                    TextBoxBackgroundImage = Yellow;
-                                    break;
-                                case "red":
-                                    TextBoxBackgroundImage = Red;
-                                    break;
                             }
-
-                            switch(name)
-                            {
-                                case "Санкт-Петербург":
-                                    TextBoxBackBackgroundImage = 'http://info.maps.yandex.net/traffic/spb/tends_200.png';
-                                    break;
-                                case "Москва":
-                                    TextBoxBackBackgroundImage = 'http://info.maps.yandex.net/traffic/moscow/tends_200.png';
-                                    break;
-                                case "Екатеринбург":
-                                    TextBoxBackBackgroundImage = 'http://info.maps.yandex.net/traffic/ekb/tends_200.png';
-                                    break;
-                                case "Киев":
-                                    TextBoxBackBackgroundImage = 'http://info.maps.yandex.net/traffic/kiev/tends_200.png';
-                                    break;
-                                default :
-                                    TextBoxBackBackgroundImage = '';
-                                    break;
-                            }
-
-                            update_tile(users[i].URIs);
-
-                            //console.log(users[i]);
-                            //console.log(level);
-                            //console.log(TextBoxBackBackgroundImage);
                         }
 
                         console.log('все обновили');
@@ -211,56 +196,59 @@ http.createServer(function (req, res) {
 
             console.log("http://geocode-maps.yandex.ru/1.x/?geocode="+longitude+","+latitude+"&format=json&kind=locality&results=1");
 
-            if(jsonobj.response.GeoObjectCollection.featureMember[0].GeoObject != undefined)
+            if(jsonobj.response.GeoObjectCollection.metaDataProperty.GeocoderResponseMetaData.found != 0)
             {
-                town = jsonobj.response.GeoObjectCollection.featureMember[0].GeoObject.name;
+                if(jsonobj.response.GeoObjectCollection.featureMember[0].GeoObject.metaDataProperty.GeocoderMetaData.AddressDetails.Country.Locality != undefined)
+                {
+                    town = jsonobj.response.GeoObjectCollection.featureMember[0].GeoObject.metaDataProperty.GeocoderMetaData.AddressDetails.Country.Locality.LocalityName;
 
-                db.users.find({URIs: live}, function(err, users) {
-                    if( err || !users.length)
-                    { // no user
-                        console.log("Такого пользователя нет - добавляем");
-                        console.log('Город: ' + town.toString());
+                    db.users.find({URIs: live}, function(err, users) {
+                        if( err || !users.length)
+                        { // no user
+                            console.log("Такого пользователя нет - добавляем");
+                            console.log('Город: ' + town.toString());
 
-                        db.users.save({name: "name",
-                            URIs: live,
-                            Latitude: latitude,
-                            Longitude: longitude,
-                            Town: town
-                        }, function(err, saved) {
-                            if( err || !saved ){
-                                console.log("User not saved");
-                                // нужно что  то сделать
-                            }
-                            else console.log("User saved");
+                            db.users.save({name: "name",
+                                URIs: live,
+                                Latitude: latitude,
+                                Longitude: longitude,
+                                Town: town
+                            }, function(err, saved) {
+                                if( err || !saved ){
+                                    console.log("User not saved");
+                                    // нужно что  то сделать
+                                }
+                                else console.log("User saved");
 
-                            count_user++;
-                        });
-                    }
-                    else users.forEach( function(User) {
-                        // пользователь есть - обновим координаты и город
-                        console.log("Такой пользователь уже есть");
-                        console.log(User);
+                                count_user++;
+                            });
+                        }
+                        else users.forEach( function(User) {
+                            // пользователь есть - обновим координаты и город
+                            console.log("Такой пользователь уже есть");
+                            console.log(User);
 
-                        db.users.update({URIs: live},{$set: {Latitude: latitude, Longitude: longitude,Town: town}}, function(err, updated) {
-                            if( err || !updated ) console.log("User not updated");
-                            else console.log("User updated");
+                            db.users.update({URIs: live},{$set: {Latitude: latitude, Longitude: longitude,Town: town}}, function(err, updated) {
+                                if( err || !updated ) console.log("User not updated");
+                                else console.log("User updated");
+                            });
                         });
                     });
-                });
+                }
             }
         });
     }).on('error', function(e) {
             console.log("Got error: " + e.message);
     });
 
-}).listen(app.get('port_phone7'),function(){
+}).listen(port_phone7,function(){
 
-        console.log('Ожидаем телефоны на порте ' + app.get('port_phone7'));
+        console.log('Ожидаем телефоны на порте ' + port_phone7);
 
     });
 
 //отправим запрос яндексу - составим базу городов
-http.get("http://api-maps.yandex.ru/services/traffic-info/1.0/?format=json",
+/*http.get("http://api-maps.yandex.ru/services/traffic-info/1.0/?format=json",
     function(res) {
     console.log("Got response: " + res.statusCode);
 
@@ -277,25 +265,12 @@ http.get("http://api-maps.yandex.ru/services/traffic-info/1.0/?format=json",
 
         for( var i = 0,length = jsonobj.GeoObjectCollection.features.length; i < length; i++ )
         {
-            db_town.town.find({name: jsonobj.GeoObjectCollection.features[i].properties.name.toString()}, function(err, town) {
-                if( err || !town.length)
-                { // no user
-                    console.log("Такого города нет - добавляем");
-                    console.log('Город: ' + town.name);
-
-                    db_town.town.save({name: jsonobj.GeoObjectCollection.features[i].properties.name.toString(), number: i}, function(err, saved) {
-                        if( err || !saved ){
-                            console.log("Town not saved");
-                            // нужно что  то сделать
-                        }
-                        else console.log("Town saved");
-                    });
+            db_town.town.save({name: jsonobj.GeoObjectCollection.features[i].properties.name.toString(), number: i}, function(err, saved) {
+                if( err || !saved ){
+                    console.log("Town not saved");
+                    // нужно что  то сделать
                 }
-                else town.forEach( function(Town) {
-                    // пользователь есть - обновим координаты и город
-                    console.log("Такой город уже есть");
-                    console.log(Town.name);
-                });
+                else console.log("Town saved");
             });
 
             console.log('Город: ' + jsonobj.GeoObjectCollection.features[i].properties.name);
@@ -305,3 +280,4 @@ http.get("http://api-maps.yandex.ru/services/traffic-info/1.0/?format=json",
 }).on('error', function(e) {
         console.log("Got error: " + e.message);
     });
+*/
